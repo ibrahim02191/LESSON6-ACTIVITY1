4b4f1f131e1c1e024f6a6025cd51abb6d60e8802
IBRAHIM <ibrahimmuzammilmohammed@gmail.com>
2025-01-30 20:41:30 +0530

    first commit

---
+++ activity1.py
@@ -0,0 +1,19 @@
+a = 10
+b = 12
+c = 0
+if a and b and c :
+    print("all the numbers have boolean value true")
+else :
+    print("atleast one number has boolean value as false")
+a = 10
+b = -10
+c = 0
+if a > 0 or b > 0 :
+    print("EITHER NUMBER IS GREATER THAN ZERO")
+else :
+    print("no number is greater than zero")
+if b > 0 or c > 0 :
+    print("EITHER OF THE NUMBER IS GREATER THAN ZERO")
+else :
+    print("NO NUMBER IS GREATER THAN ZERO")
+    
